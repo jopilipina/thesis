ac60cf345e2c52b70d11feed02e148582913c5ce
jiggerangelo <jiggerpilipina@gmail.com>
2018-10-12 05:42:48 +0800

    Add python file

---
+++ test.py
@@ -0,0 +1,41 @@
+import os
+import glob
+import re
+
+def main(text,g):
+	a=re.compile(".*abstract.*", re.IGNORECASE)
+	b=re.compile(".*background.*", re.IGNORECASE)
+	c=re.compile(".*summary.*", re.IGNORECASE)
+	for i in range(0,len(text)):
+		if a.match(text[i]) or b.match(text[i]) or c.match(text[i]):
+			count=0
+			for j in range(i,len(text)):
+				g.write(text[j])
+				if text[j]=='':
+					count+=1
+				if count==2:
+					return
+
+
+
+
+for file in sorted(glob.glob('*.pdf')):
+	command='pdf2txt.py -o temp.txt '+file
+
+	os.system(command)
+
+	f=open('temp.txt')
+	g=open('output.txt','a')
+
+	g.write(file)
+	g.write('\n')
+
+	text = [line.strip('\n') for line in f]
+	
+	main(text,g)
+
+	g.write('\n')
+	g.write('\n')
+
+	f.close()
+	g.close()
