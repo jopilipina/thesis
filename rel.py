00fc49533970339b9f5a80fab67187b73d8f9cb9
jopilipina <jopilipina@up.edu.ph>
2019-04-25 15:28:27 +0800

    Add kindred test files

---
+++ rel.py
@@ -0,0 +1,15 @@
+import kindred
+trainCorpus = kindred.bionlpst.load('2016-BB3-event-train')
+devCorpus = kindred.bionlpst.load('2016-BB3-event-dev')
+
+predictionCorpus = devCorpus.clone()
+predictionCorpus.removeRelations()
+
+classifier = kindred.RelationClassifier()
+classifier.train(trainCorpus)
+classifier.predict(predictionCorpus)
+
+f1score = kindred.evaluate(devCorpus, predictionCorpus, metric='f1score')
+
+for i in predictionCorpus.getRelations():
+  print(i)
