473d41544987cfebd8fe3d814f6da98ae12e0a9e
Jade De Guzman <njadedeguzman@gmail.com>
2019-05-06 11:57:46 +0800

    with txt files

--- kindred/annotate.py
+++ kindred/annotate.py
@@ -1,9 +1,10 @@
 import nltk.data
 import os
+import json
 
 tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
 fp = open("test_corpus.txt")
-wf = open("answer.txt", "w")
+wf = open("answer.json", "w")
 data = fp.read()
 # print '\n-----\n'.join(tokenizer.tokenize(data))
 sentences = tokenizer.tokenize(data)
@@ -11,8 +12,9 @@ done = ""
 for i in sentences:
 	print(sentences.index(i)+1)
 	print(i) # print sentence on terminal
-	wf.write('{\n') 															# {
-	wf.write('\t"text: "'+str(i)+'",\n') 										# "text": "sentence",
+	wf.write('{\n') 			
+	datastore = '\t"text: "'+str(i)+'",\n'
+	json.dumps(datastore, wf)												# {									# "text": "sentence",
 	wf.write('\t"denotations":\n') 												# "denotations":
 	word = raw_input('word: ')
 	while word!="x":
