635eb91920e6d7d286dd569fa886e739e0f09a0a
Jade De Guzman <njadedeguzman@gmail.com>
2019-05-06 10:36:33 +0800

    annotate.py - manual annotation on terminal. relations will be manually edited on text once done

---
+++ kindred/annotate.py
@@ -0,0 +1,34 @@
+import nltk.data
+import os
+
+tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
+fp = open("test_corpus.txt")
+wf = open("answer.txt", "w")
+data = fp.read()
+# print '\n-----\n'.join(tokenizer.tokenize(data))
+sentences = tokenizer.tokenize(data)
+done = ""
+for i in sentences:
+	print(sentences.index(i)+1)
+	print(i) # print sentence on terminal
+	wf.write('{\n') 															# {
+	wf.write('\t"text: "'+str(i)+'",\n') 										# "text": "sentence",
+	wf.write('\t"denotations":\n') 												# "denotations":
+	word = raw_input('word: ')
+	while word!="x":
+		wf.write('\t\t[{"id":"T1", "obj":"'+word+'",\n')							# [{"id":"T1", "obj":"disease",
+		first = i.find(word)
+		last = int(first)+len(word)
+		wf.write('\t\t"span":{"begin":'+str(first)+',"end":'+str(last)+'}},\n')		# "span":{"begin":4,"end":21}},
+		print('First:', first)
+		print('Last:', last)
+		word = raw_input('word: ')
+	word = ""
+	wf.write('\t"relations"\n')													# "relations"
+	wf.write('}\n') 															# }
+	os.system('clear')
+fp.close()
+wf.close()
+# text = ""
+# text.find("word")
+# len(word)
--- kindred/rel.py
+++ kindred/rel.py
@@ -1,1 +1,17 @@
-corpus = kindred.loadDir(dataFormat='json',directory='example/')
+import kindred
+import argparse
+import os
+
+trainCorpus = kindred.loadDir(dataFormat='json',directory='corpus/')
+
+predictionCorpus = devCorpus.clone()
+predictionCorpus.removeRelations()
+
+classifier = kindred.RelationClassifier()
+classifier.train(trainCorpus)
+classifier.predict(predictionCorpus)
+
+f1score = kindred.evaluate(devCorpus, predictionCorpus, metric='f1score')
+
+for i in predictionCorpus.getRelations():
+  print(i)
