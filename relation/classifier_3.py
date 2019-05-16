240bd19fc3271def57349b0a61df8321f739bb5d
Jade De Guzman <nrdeguzman@up.edu.ph>
2019-05-16 19:12:17 +0800

    running retesting

--- relation/classifier_3.py
+++ relation/classifier_3.py
@@ -6,7 +6,7 @@ import csv
 # trainCorpus = kindred.load(dataFormat='json',path='db/2/train')
 # devCorpus = kindred.load(dataFormat='json',path='db/2/train/')
 
-with open('three_types.csv', mode='w') as csv_file:
+with open('3_types.csv', mode='w') as csv_file:
 	fieldnames = ['iteration_num', 'svm', 'decision_trees', 'neural_net']
 	writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
 
@@ -19,11 +19,13 @@ with open('three_types.csv', mode='w') as csv_file:
 	avg_nn = 0
 
 	count = 0
-	iter_num = 5
+	iter_num = 1000
 
 	print("-------------3 CLASSES------------")
 
 	while count < iter_num:
+		print("------",count,"------")
+
 		trainCorpus,devCorpus = Corpus.split(trainFraction=0.9)
 
 		predictionCorpus = devCorpus.clone()
@@ -32,32 +34,35 @@ with open('three_types.csv', mode='w') as csv_file:
 		classifier = kindred.RelationClassifier()
 		classifier.train(trainCorpus)
 		classifier.predict(predictionCorpus)
-		svmf1score = kindred.evaluate(devCorpus, predictionCorpus, metric='f1score',display=True)
+		svmf1score = kindred.evaluate(devCorpus, predictionCorpus, metric='f1score',display=False)
 		print("svm:\t", svmf1score)
-
 		avg_svm = avg_svm+svmf1score
 
 		classifier = kindred.RelationClassifier(classifierType='DCT')
 		classifier.train(trainCorpus)
 		classifier.predict(predictionCorpus)
-		dctf1score = kindred.evaluate(devCorpus, predictionCorpus, metric='f1score',display=True)
+		dctf1score = kindred.evaluate(devCorpus, predictionCorpus, metric='f1score',display=False)
 		print("dct:\t", dctf1score)	
 		avg_dct = avg_dct+dctf1score
 
 		classifier = kindred.RelationClassifier(classifierType='NN')
 		classifier.train(trainCorpus)
 		classifier.predict(predictionCorpus)
-		nnf1score = kindred.evaluate(devCorpus, predictionCorpus, metric='f1score',display=True)
+		nnf1score = kindred.evaluate(devCorpus, predictionCorpus, metric='f1score',display=False)
 		print("nn:\t", nnf1score)	
 		avg_nn = avg_nn+nnf1score
 
-		writer.writerow({'iteration_num': iter_num, 'svm': svmf1score, 'decision_trees': dctf1score, 'neural_net': nnf1score})
+		writer.writerow({'iteration_num': count, 'svm': svmf1score, 'decision_trees': dctf1score, 'neural_net': nnf1score})
 
 		count += 1
 
-	print("average of SVM:", avg_svm/iter_num)
-	print("average of DCT:", avg_dct/iter_num)
-	print("average of Neural Networks:", avg_nn/iter_num)
+	avg_svm = avg_svm/iter_num
+	avg_dct = avg_dct/iter_num
+	avg_nn = avg_nn/iter_num
+
+	print("average of SVM:", avg_svm)
+	print("average of DCT:", avg_dct)
+	print("average of Neural Networks:", avg_nn)
 
 
 	writer.writerow({'iteration_num': iter_num, 'svm': avg_svm, 'decision_trees': avg_dct, 'neural_net': avg_nn})
--- relation/classifier_5.py
+++ relation/classifier_5.py
@@ -3,45 +3,66 @@ import argparse
 import os
 import csv
 
-Corpus = kindred.load(dataFormat='json',path='db/1')
+# trainCorpus = kindred.load(dataFormat='json',path='db/2/train')
+# devCorpus = kindred.load(dataFormat='json',path='db/2/train/')
 
-avg_svm = 0
-avg_dct = 0
-avg_nn = 0
+with open('5_types.csv', mode='w') as csv_file:
+	fieldnames = ['iteration_num', 'svm', 'decision_trees', 'neural_net']
+	writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
 
-count = 0
-iter_num = 5
+	writer.writeheader()
 
-print("-------------5 CLASSES------------")
-while count < iter_num:
-	trainCorpus,devCorpus = Corpus.split(trainFraction=0.9)
+	Corpus = kindred.load(dataFormat='json',path='db/1')
 
-	predictionCorpus = devCorpus.clone()
-	predictionCorpus.removeRelations()
+	avg_svm = 0
+	avg_dct = 0
+	avg_nn = 0
 
-	classifier = kindred.RelationClassifier()
-	classifier.train(trainCorpus)
-	classifier.predict(predictionCorpus)
-	svmf1score = kindred.evaluate(devCorpus, predictionCorpus, metric='f1score',display=True)
-	print("svm:\t", svmf1score)
-	avg_svm = avg_svm+svmf1score
+	count = 0
+	iter_num = 1000
 
-	classifier = kindred.RelationClassifier(classifierType='DCT')
-	classifier.train(trainCorpus)
-	classifier.predict(predictionCorpus)
-	dctf1score = kindred.evaluate(devCorpus, predictionCorpus, metric='f1score',display=True)
-	print("dct:\t", dctf1score)	
-	avg_dct = avg_dct+dctf1score
+	print("-------------5 CLASSES------------")
 
-	classifier = kindred.RelationClassifier(classifierType='NN')
-	classifier.train(trainCorpus)
-	classifier.predict(predictionCorpus)
-	nnf1score = kindred.evaluate(devCorpus, predictionCorpus, metric='f1score',display=True)
-	print("nn:\t", nnf1score)	
-	avg_nn = avg_nn+nnf1score
+	while count < iter_num:
+		print("------",count,"------")
 
-	count += 1
+		trainCorpus,devCorpus = Corpus.split(trainFraction=0.9)
 
-print("average of SVM:", avg_svm/iter_num)
-print("average of DCT:", avg_dct/iter_num)
-print("average of Neural Networks:", avg_nn/iter_num)
+		predictionCorpus = devCorpus.clone()
+		predictionCorpus.removeRelations()
+
+		classifier = kindred.RelationClassifier()
+		classifier.train(trainCorpus)
+		classifier.predict(predictionCorpus)
+		svmf1score = kindred.evaluate(devCorpus, predictionCorpus, metric='f1score',display=False)
+		print("svm:\t", svmf1score)
+		avg_svm = avg_svm+svmf1score
+
+		classifier = kindred.RelationClassifier(classifierType='DCT')
+		classifier.train(trainCorpus)
+		classifier.predict(predictionCorpus)
+		dctf1score = kindred.evaluate(devCorpus, predictionCorpus, metric='f1score',display=False)
+		print("dct:\t", dctf1score)	
+		avg_dct = avg_dct+dctf1score
+
+		classifier = kindred.RelationClassifier(classifierType='NN')
+		classifier.train(trainCorpus)
+		classifier.predict(predictionCorpus)
+		nnf1score = kindred.evaluate(devCorpus, predictionCorpus, metric='f1score',display=False)
+		print("nn:\t", nnf1score)	
+		avg_nn = avg_nn+nnf1score
+
+		writer.writerow({'iteration_num': count, 'svm': svmf1score, 'decision_trees': dctf1score, 'neural_net': nnf1score})
+
+		count += 1
+		
+	avg_svm = avg_svm/iter_num
+	avg_dct = avg_dct/iter_num
+	avg_nn = avg_nn/iter_num
+
+	print("average of SVM:", avg_svm)
+	print("average of DCT:", avg_dct)
+	print("average of Neural Networks:", avg_nn)
+
+
+	writer.writerow({'iteration_num': iter_num, 'svm': avg_svm, 'decision_trees': avg_dct, 'neural_net': avg_nn})
